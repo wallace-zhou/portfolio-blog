221dc5889119e47063269f5515982275ddfc1576
Wallace Zhou <walcezu@umich.edu>
2026-01-18 23:53:09 -0500

    coverted to astro

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = '光影詩 | Photography Portfolio';
+export const SITE_DESCRIPTION = 'A curated collection of fine art photography exploring the intersection of light and shadow.';
