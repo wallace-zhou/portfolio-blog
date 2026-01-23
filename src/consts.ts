8a02c84967d6afc005794d53eff52804b85145bd
Wallace Zhou <walcezu@umich.edu>
2026-01-22 23:51:00 -0500

    improved hero styling

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Wallace Z ';
+export const SITE_TITLE = 'WALLACE ZHOU';
 export const SITE_DESCRIPTION = 'My stories and work as you follow me on my journey to become a better photographer.';
